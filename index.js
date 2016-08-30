26080d7f53abaf3e38189f71b8ef3978d7903afe
Tom Coleman <tom@thesnail.org>
2016-08-30 15:52:19 +1000

    First version seems to be working

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports["apollo-client"] = require("apollo-client");
+module.exports["graphql"] = require("graphql");
+module.exports["graphql-tag"] = require("graphql-tag");
+module.exports["graphql-tools"] = require("graphql-tools");
+module.exports["lodash"] = require("lodash");
+module.exports["react"] = require("react");
+module.exports["react-apollo"] = require("react-apollo");
+module.exports["react-dom"] = require("react-dom");
